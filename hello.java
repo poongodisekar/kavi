3dc2e520d3ec7e5129c9631802d91d52dbd972e2
poongodisekar <anjalisekar1996@gmail.com>
2017-07-04 15:47:58 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class hello{
+public static void main(String args[]){
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+Scanner sc=new Scanner(System.in);
+String s;
+int n;
+for(int i=0;i<=4;i++)
+{
+System.out.println("Hello");
+}
+}
+}
