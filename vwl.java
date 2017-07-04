074ee28004cb516768743ef348da5883aa95e24d
poongodisekar <anjalisekar1996@gmail.com>
2017-07-04 15:16:47 +0530

    Create vwl.java

---
+++ vwl.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class vwl{
+public static void main(String args[])
+{
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+		System.out.println("Entered character "+ch+" is  Vowel"); 
+		}
+		else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+    
+    {
+			System.out.println("Entered character "+ch+" is Consonent");
+      }
+		      else
+          {
+			System.out.println("Not an alphabet");		
+	}
+	}
+  }
